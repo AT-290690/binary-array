bf3b2ee7358a015e1d130893f9055a8b42129262
anthony163 <anthony@techpods.co>
2021-10-15 21:13:37 +0300

    Remove list

--- src/BinaryArray.js
+++ src/BinaryArray.js
@@ -1,6 +1,14 @@
-import { BinaryList } from './BinaryList.js';
+export class BinaryArray {
+  _offsetRight = 0;
+  _offsetLeft = 0;
+  left = [];
+  right = [];
+  size = 0;
 
-export class BinaryArray extends BinaryList {
+  constructor(initial = []) {
+    this.init(initial);
+  }
+
   static isBinaryArray(entity) {
     return entity instanceof BinaryArray;
   }
@@ -17,10 +25,6 @@ export class BinaryArray extends BinaryList {
     return new BinaryArray(items);
   }
 
-  constructor(initial) {
-    super(initial);
-  }
-
   get first() {
     return this.get(0);
   }
@@ -31,6 +35,98 @@ export class BinaryArray extends BinaryList {
 
   get pivot() {
     return this.right[0];
+  }
+
+  _access(key) {
+    const index = this.abs(key);
+    return key >= 0 ? this.right[index] : this.left[index];
+  }
+
+  _add(key, value) {
+    const index = this.abs(key);
+    if (key >= 0) this.right[index] = value;
+    else this.left[index] = value;
+    this.size++;
+  }
+
+  _delete(key) {
+    if (this.size === 1) {
+      this.left = [];
+      this.right = [];
+      this.size = 0;
+      this._offsetLeft = 0;
+      this._offsetRight = 0;
+      return;
+    }
+    if (key < 0 && this.left.lenght > 0) this.left.length--;
+    if (key >= 0 && this.right.lenght > 0) this.right.length--;
+    this.size--;
+  }
+
+  abs(key) {
+    return key < 0 ? key * -1 : key;
+  }
+
+  init(initial) {
+    if (
+      initial &&
+      !Array.isArray(initial) &&
+      typeof initial[Symbol.iterator] === 'function'
+    ) {
+      initial = [...initial];
+    }
+    if (this.size) this.clear();
+    const half = Math.floor(initial.length / 2);
+    for (let i = half - 1; i >= 0; i--) this.addToLeft(initial[i]);
+    for (let i = half; i < initial.length; i++) this.addToRight(initial[i]);
+  }
+
+  get(index) {
+    return this._access(index + this._offsetLeft);
+  }
+
+  clear() {
+    this.right = [];
+    this.left = [];
+    this.size = 0;
+    this._offsetLeft = 0;
+    this._offsetRight = 0;
+  }
+
+  addToLeft(item) {
+    this._add(--this._offsetLeft, item);
+  }
+
+  addToRight(item) {
+    this._add(this._offsetRight++, item);
+  }
+
+  removeFromLeft() {
+    this.size && this._delete(this._offsetLeft++);
+  }
+
+  removeFromRight() {
+    this.size && this._delete(--this._offsetRight);
+  }
+
+  vectorIndexOf(index) {
+    const key = index + this._offsetLeft;
+    return key < 0 ? [key * -1, -1] : [key, 1];
+  }
+
+  set(key, value) {
+    const [index, direction] = this.vectorIndexOf(key);
+    return direction >= 0
+      ? (this.right[index] = value)
+      : (this.left[index] = value);
+  }
+
+  [Symbol.iterator] = function* () {
+    for (let i = 0; i < this.size; i++) yield this.get(i);
+  };
+
+  toArray() {
+    return [...this];
   }
 
   at(index) {
@@ -52,12 +148,17 @@ export class BinaryArray extends BinaryList {
   }
 
   pop() {
+    if (this._offsetRight === 0) {
+      this.balance();
+    }
     const last = this.last;
     this.removeFromRight();
     return last;
   }
-
   shift() {
+    if (this._offsetLeft === 0) {
+      this.balance();
+    }
     const first = this.first;
     this.removeFromLeft();
     return first;
